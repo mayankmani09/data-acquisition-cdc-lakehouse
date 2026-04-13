1099c84e4c8f6d7cf8c1f3fce3f80fcdbb2ac00f
mayankmani09 <mayankmani09@gmail.com>
2026-04-13 12:56:37 -0400

    Create constants.py

---
+++ src/common/constants.py
@@ -0,0 +1,10 @@
+CDC_OP_INSERT = "I"
+CDC_OP_UPDATE = "U"
+CDC_OP_DELETE = "D"
+
+SOURCE_A = "source_a"
+SOURCE_B = "source_b"
+
+DOMAIN_CUSTOMERS = "customers"
+DOMAIN_ORDERS = "orders"
+DOMAIN_PAYMENTS = "payments"
